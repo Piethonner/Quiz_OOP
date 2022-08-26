54c74c4f2f00c8732aee6fa2a31936acb7595bea
Piethonner <110135307+Piethonner@users.noreply.github.com>
2022-08-26 22:25:39 +0200

    Add files via upload

--- quiz_brain.py
+++ quiz_brain.py
@@ -6,15 +6,18 @@ class QuizBrain:
         self.score = 0
 
     def questions_left(self):
+        """Returns True if there are any questions left, False if quiz is out of questions."""
         return self.q_number < len(self.all_questions)
 
     def next_question(self):
+        """Counts question number, gathers input T/F."""
         current_question = self.all_questions[self.q_number]
         self.q_number += 1
         user_answer = input(f"Q.{self.q_number}: {current_question.text} (True/False): ")
         self.check_answer(user_answer, current_question.answer)
 
     def check_answer(self, user_answer, correct_answer):
+        """Checks if answer is correct."""
         if user_answer.lower() == correct_answer.lower():
             print("Correct answer!")
             self.score += 1
